ddcd56b3ccddc7dbd96f1deb8f8e5123bdcdcad9
Alireza Hamid <alirezahamid20@gmail.com>
2020-05-13 16:39:08 +0430

    Updates

---
+++ precache-manifest.6908f77094a58e1785e0c7a91a63f0a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e43309f59928621b4861b2c31a9e2fa2",
+    "url": "/covid-19-reactJs/index.html"
+  },
+  {
+    "revision": "78ad96e9161f01fb5fb4",
+    "url": "/covid-19-reactJs/static/css/main.9eba056d.chunk.css"
+  },
+  {
+    "revision": "fd94aff940327b03b4e4",
+    "url": "/covid-19-reactJs/static/js/2.ffbff3d8.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-reactJs/static/js/2.ffbff3d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78ad96e9161f01fb5fb4",
+    "url": "/covid-19-reactJs/static/js/main.9aa43b13.chunk.js"
+  },
+  {
+    "revision": "77f14ed34f709dc3dd21",
+    "url": "/covid-19-reactJs/static/js/runtime-main.43d07a39.js"
+  }
+]);
